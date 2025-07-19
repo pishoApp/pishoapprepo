847284a1b28daea4dfa36a63ed72c13264cfcfff
RG15g <razvisaapplication@gmail.com>
2025-07-19 16:32:30 +0300

    new relaesse

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -32,9 +32,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "30b4a06a262758cb5d42c78f5078f8da
 "icons/icon-512.png": "d0338b841c33c52e53cb6b94e6715ccd",
 "icons/icon-maskable-192.png": "d0338b841c33c52e53cb6b94e6715ccd",
 "icons/icon-maskable-512.png": "d0338b841c33c52e53cb6b94e6715ccd",
-"index.html": "ec3d11a4b6d682ded48e4ac6ce9f3786",
-"/": "ec3d11a4b6d682ded48e4ac6ce9f3786",
-"main.dart.js": "c64e51b40ed555f93abc9df5c9bf034f",
+"index.html": "60ea0ae1383f23f6573872399d24bda4",
+"/": "60ea0ae1383f23f6573872399d24bda4",
+"main.dart.js": "84a367f4cc0cfc4c55afd6516176af4e",
 "manifest.json": "2d310b42c126dc98dac3977213af9a54",
 "version.json": "e57b5f4728a1e98c197e037cbee22728"};
 // The application shell files that are downloaded before a service worker can
